79c316b812a7eca78ac8cfc1dc5b4958275c9829
Peter Mok <43172966+petermok-1213@users.noreply.github.com>
2021-10-09 18:23:51 +0800

    PriorityQueue now use dictionary, instead of list

--- data_structure.py
+++ data_structure.py
@@ -1,9 +1,9 @@
-from math import inf
+from math import e, inf
 
 class PriorityQueue():
 
     def __init__(self) -> None:
-        self.queue = []     # [ (key0, weight0), (key1, weight1), ...]
+        self.queue = {}     # [ (key0, weight0), (key1, weight1), ...]
 
     # return if the list is empty, O(1) time
     def is_empty(self):
@@ -11,7 +11,14 @@ class PriorityQueue():
 
     # insert key, weight into the list, O(1) time
     def insert(self, key: int, weight: int) -> None:
-        self.queue.append((key, weight))
+        self.queue[key] = weight
+
+    # modify the weight at the key, raise exception if key not found
+    def update(self, key: int, weight: int) -> None:
+        if key in self.queue:
+            self.queue[key] = weight
+        else:
+            raise KeyError("Key not found")
 
     # pop the key with the least weight, return -1 if the list is empty
     # O(n) time
@@ -19,39 +26,22 @@ class PriorityQueue():
         if not self.is_empty():
             min_weight = inf
             min_key = None
-            for tup in self.queue:  # tup[0] -> key, tup[1] -> weight
-                if min_weight > tup[1]:
-                    min_key = tup[0]
-                    min_weight = tup[1]
-            self.queue.remove((min_key, min_weight))
+            for key, weight in self.queue.items():        
+                if min_weight > weight:   
+                    min_key = key
+                    min_weight = weight      
+            self.queue.pop(min_key)
             return min_key
         else:
             return -1
 
-    # modify the weight at the key, raise exception if key not found
-    def update(self, key: int, weight: int) -> None:
-        for i in range(len(self.queue)):
-            if self.queue[i][0] == key:
-                self.queue[i] = (key, weight)
-            return
-        raise KeyError("Key not found in queue")   # somehow this doesnt work?
-
 
 if __name__ == "__main__":
-
-    # testing the insert and pop method
-    Q = PriorityQueue()
-    Q.insert(1, 10)
-    Q.insert(2, 5)
-
-    print(Q.pop() == 2)
-    print(Q.pop() == 1)
-    print(Q.pop() == -1)
-
-    # testing the update method
-    Q = PriorityQueue()
-    Q.insert(5, 10)
-    Q.update(5, 8)
-    print(Q.queue[0][1] == 8)
-    print(Q.update(6, 10))
-    print(Q.queue[0][1])
+    
+    q = PriorityQueue()
+    q.insert(1, 10)
+    q.insert(2, 5)
+    print(q.pop() == 2)
+    q.update(1, 4)
+    print(q.queue[1]==4)
+    q.update(2, 10)
